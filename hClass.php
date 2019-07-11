c2b8f5efb11dce5fcf42b9f5500f3bcbea64d604
lorenzocrq <l.cerqua@wematica.it>
2019-07-11 10:46:38 +0200

    last commit

---
+++ hClass.php
@@ -0,0 +1,15 @@
+<?php
+
+class Hclass{
+
+    private $variabile;
+
+    public function __construct()
+    {
+        $this->variabile="ciao";
+    }
+    public function getVariabile(){
+        return $this->variabile;
+    }
+
+}
